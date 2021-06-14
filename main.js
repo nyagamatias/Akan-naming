6273490689fe48ff6de6f9f0d50255bf07d215e6
nyagamatias <nyagamatias@gmail.com>
2021-06-14 13:30:36 +0300

    add fill out form

--- main.js
+++ main.js
@@ -1,108 +1,103 @@
-function submit() {
-    const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    const maleNames = ["kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-    const femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"]
-    const month = parseInt(document.getElementById("month").value)
-    const name = (document.getElementById("name").value)
-    const year = parseInt(document.getElementById("year").value)
-    const day = parseInt(document.getElementById("day").value)
-    const dateOfBirth = new Date(year + "." + month + "." + day)
-    const answer = dateOfBirth.getDay()
-    const output = document.getElementById("output")
-    const male = document.getElementById("male")
-    const female = document.getElementById("female")
-    if ((year < 0) || (year > 2021)) {
-        output.style.background = "blue"
-        output.style.color = "red"
-        output.innerHTML = "Hello " + " " + name + " " + " please enter a valid year! "
-    } else if ((month < 1) || (month > 12)) {
-        output.style.background = "blue"
-        output.style.color = "red"
-        output.innerHTML = "Hello" + " " + name + " " + " please enter a valid month! "
-    } else if (day < 0 || day > 31) {
-        output.style.background = "blue"
-        output.style.color = "red"
-        output.innerHTML = "Hello" + " " + name + " " + " please enter a valid day! "
+function MakeArray(n) {
+    this.length = n;
+    for (var i = 1; i <=n; i++) {
+      this[i] = 0;
     }
-    if (female.checked && year > 1969 && year < 2020 && month > 0 && month < 12 && day > 0 && day < 31) {
-        output.style.background = "yellow"
-        output.innerHTML = "Hello" + " " + name + ",   " + "you were born on a" + " " + daysOfTheWeek[answer] + "." + " " + "Suprise!!, your Akan name is:" + femaleNames[answer]
-    } else if (male.checked && year > 0 && year < 2020 && month > 0 && month < 12 && day > 0 && day < 31) {
-        output.style.background = "blue"
-        output.innerHTML = "Hello" + " " + name + ",   " + "you were born on a" + " " + daysOfTheWeek[answer] + "." + " " + "Suprise!!, your Akan name is:" + maleNames[answer]
-    } else if (female.checked && year > 0 && year < 2020 && month > 0 && month < 12 && day > 0 && day < 31) {
-        output.style.background = "yellow"
-        output.innerHTML = "Hello" + " " + name + ",   " + "you were born on a" + " " + daysOfTheWeek[answer] + "." + " " + "Suprise!!, your Akan name is:" + femaleNames[answer]
+ }
+ //male Initialize Days of Week Array
+ days = new MakeArray(7);
+ days[0] = "Kwame"
+ days[1] = "Kwasi"
+ days[2] = "Kwadwo"
+ days[3] = "Kwabena"
+ days[4] = "Kwaku"
+ days[5] = "Yaw"
+ days[6] = "Kofi"
+ //female Initialize Days of Week Array
+ day = new MakeArray(7);
+ day[0] = "Ama"
+ day[1] = "Akosua"
+ day[2] = "Adwoa"
+ day[3] = "Abenaa"
+ day[4] = "Akua"
+ day[5] = "Yaa"
+ day[6] = "Afua"
+ //Initialize Months Array
+ months = new MakeArray(12);
+ months[1] = "January"
+ months[2] = "February"
+ months[3] = "March"
+ months[4] = "April"
+ months[5] = "May"
+ months[6] = "June"
+ months[7] = "July"
+ months[8] = "August"
+ months[9] = "September"
+ months[10] = "October"
+ months[11] = "November"
+ months[12] = "December"
+ //Day of Week Function
+ function compute(form) {
+    var val1 = parseInt(form.day.value, 10)
+    if ((val1 < 0) || (val1 > 31)) {
+       alert("Day is out of range")
     }
-}
-document.querySelector("#details").addEventListener("submit", function(event) {
-    event.preventDefault();
-    submit();
-})
-(function($) {
-    // Remove no-js class
-    $('html').removeClass('no-js');
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
-    // Create timeline
-    $('#experience-timeline').each(function() {
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
-            }
-        });
-    });
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
-    // Load additional projects
-    $('#view-more-projects').click(function(e) {
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
-})(jQuery);
+    var val2 = parseInt(form.month.value, 10)
+    if ((val2 < 0) || (val2 > 12)) {
+       alert("Month is out of range")
+    }
+    var val2x = parseInt(form.month.value, 10)
+    var val3 = parseInt(form.year.value, 10)
+    if (val3 < 1900) {
+       alert("You're that old!")
+    }
+    if (val2 == 1) {
+       val2x = 13;
+       val3 = val3-1
+    }
+    if (val2 == 2) {
+       val2x = 14;
+       val3 = val3-1
+    }
+    var val4 = parseInt(((val2x+1)*3)/5, 10)
+    var val5 = parseInt(val3/4, 10)
+    var val6 = parseInt(val3/100, 10)
+    var val7 = parseInt(val3/400, 10)
+    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+    var val9 = parseInt(val8/7, 10)
+    var val0 = val8-(val9*7)
+    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+    form.result2.value = days[val0]
+ }
+ function tomm(form) {
+    var val1 = parseInt(form.day.value, 10)
+    if ((val1 < 0) || (val1 > 31) || (val1 = null) || (val1 = '')) {
+       alert("Day is out of range")
+    }
+    var val2 = parseInt(form.month.value, 10)
+    if ((val2 < 0) || (val2 > 12) || (val1 = null) || (val1 = '')) {
+       alert("Month is out of range")
+    }
+    var val2x = parseInt(form.month.value, 10)
+    var val3 = parseInt(form.year.value, 10)
+    if (val3 < 1900) {
+       alert("You're too old!")
+    }
+    if (val2 == 1) {
+       val2x = 13;
+       val3 = val3-1
+    }
+    if (val2 == 2) {
+       val2x = 14;
+       val3 = val3-1
+    }
+    var val4 = parseInt(((val2x+1)*3)/5, 10)
+    var val5 = parseInt(val3/4, 10)
+    var val6 = parseInt(val3/100, 10)
+    var val7 = parseInt(val3/400, 10)
+    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+    var val9 = parseInt(val8/7, 10)
+    var val0 = val8-(val9*7)
+    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+    form.result2.value = day[val0]
+ }
